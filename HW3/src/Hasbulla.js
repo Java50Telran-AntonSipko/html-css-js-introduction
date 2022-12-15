cada94a46e17442d5d8dab095376e2fe4620830e
Anton <sipcoanton@gmail.com>
2022-12-15 15:31:00 +0200

    Adding hide button+restyling fpictures directions

--- HW3/src/Hasbulla.js
+++ HW3/src/Hasbulla.js
@@ -1,15 +1,23 @@
-const detailsImage=document.querySelector=(".details-image");
-const detailsTitle=document.querySelector=(".details-title");
-const anchorElements=document.querySelectorAll(".thumbnails-anchor")
+const detailsImage = document.querySelector(".details-image");
+const detailsTitle = document.querySelector(".details-title");
+const anchorElements = document.querySelectorAll(".thumbnails-anchor")
+const mainElement=document.querySelector("main")
 function setDetails(anchor) {
-    const dataImage=anchor.getAttribute("data-details-image");
-    detailsImage.src=dataImage;
-    detailsTitle.innerHTML=anchor.getAttribute("data-details-title");
+    const dataImage = anchor.getAttribute("data-details-image");
+    detailsImage.src = dataImage;
+    detailsTitle.innerHTML = anchor.getAttribute("data-details-title");
+    showDetails();
 
 }
-for(let i=0;i<anchorElements.length;i++){
-    anchorElements[i].addEventListener("click",function(){
+for (let i = 0; i < anchorElements.length; i++) {
+    anchorElements[i].addEventListener("click", function() {
         setDetails(anchorElements[i]);
     })
 }
-
+const HIDDEN="hidden"
+function hideDetails(){
+    mainElement.classList.add(HIDDEN);
+}
+function showDetails(){
+    mainElement.classList.remove(HIDDEN);
+}
