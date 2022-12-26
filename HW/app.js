01d8dfc5ccf3bacb95a203e35a437c48e2770bc4
Anton <sipcoanton@gmail.com>
2022-12-26 19:15:35 +0200

    HW

--- HW/app.js
+++ HW/app.js
@@ -20,10 +20,16 @@ function getDigitsSum(number) {
 console.log(getDigitsSum(number));//expected return 10
 console.log(getDigitsSum(number2));//expected return 6
 console.log(getDigitsSum(number3));//expected return 6
-let expr = "9000 / ((10 + 20) ** 2)";
+let expr = "9000 / ((10 + 20) ** 2";
 let expr2 = ("9 + 100 / 2");
 function computeExpression(expressionStr) {
-    let res = eval(expressionStr);
+    let res;
+    try{
+        res = eval(expressionStr);
+
+    }catch(e){
+        res="Wrong expression";
+    }
     return res;
 }
 console.log(computeExpression(expr));//expected return 10
@@ -35,7 +41,7 @@ function printAnanas() {
     let s = "S";
     let res = a + c / 100 + a + s;
     console.log(res);
-
+    
 }
 console.log(printAnanas());//expected return ANaNAS
 
@@ -60,6 +66,12 @@ function reverse(number) {
     console.log(reverse(number2));//expected return 321
     console.log(reverse(number4));//expected return -321
 
+    let n=4;
+    let count=0;
+    while(n--){
+        count++;
+    }
+    let d;
 
 
 
---
+++ HW4/HW#14.js
@@ -0,0 +1,73 @@
+function myParseInt(str, base) {
+    base = base || 10;
+    let res = 0;
+    for(let i = 0; i < str.length; i++) {
+            res = res * base + getCode(str[i]);
+    }
+    return res;
+}
+function getCode(symbol) {
+    symbol = symbol.toLowerCase();
+    const codeA = 'a'.charCodeAt();
+    const res = symbol <= '9'? +symbol : symbol.charCodeAt() - codeA + 10;
+    return res;
+}
+let str1 = "ff";
+let str2 = "123";
+let str22 = "Java";
+let str3 = "123m"
+let str4 = "123.5"
+let str5="-123"
+let num = parseInt(str1, 16);
+let myNum = myParseInt(str1, 16);
+num = parseInt(str2);
+myNum = myParseInt(str2);
+num = parseInt(str22, 36);
+myNum = myParseInt(str22, 36);
+num = parseInt(str3);
+myNum = myParseInt(str3);
+num = parseInt(str4);
+myNum = myParseInt(str4);
+let number = 255;
+let str = "" + number;
+str = number.toString(36);
+num=parseInt(str5);
+myNum=myParseInt(str5);
+
+
+function myToString(number, base) {
+    let res = '';
+    base = base || 10;
+    number = Math.trunc(Math.abs(number));
+    do {
+        const digit = number % base;
+        const symbol = getSymbol(digit);
+        res = symbol + res;
+        number = Math.trunc(number / base);
+
+
+    }while(number);
+    return res;
+
+}
+function getSymbol(digit) {
+    const codeA = 'a'.charCodeAt();
+    let symbol;
+   if(digit < 10) {
+    symbol = "" + digit;
+   } else {
+     const codeAscii = digit - 10 + codeA;
+     symbol = String.fromCharCode(codeAscii);
+   }
+   return symbol;
+
+}
+let num100 = 990500;
+let str100 = num100.toString();
+let myStr100 = myToString(num100);
+str100 = num100.toString(36);
+myStr100 = myToString(num100, 36);
+num100 = 123.45;
+str100 = num100.toString(16);
+myStr100 = myToString(num100, 16);
+
